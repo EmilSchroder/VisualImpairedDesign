fd15bcea5a7bde4f7591ea451de1ad70f78f6f6f
EmilSchroder <eschro66@gmail.com>
2018-10-31 11:50:06 +1300

    app rendering in localhost

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
+import { HashRouter as Router, Route, Link } from 'react-router-dom'
 
-const App = () => {
-    return (
-        <div>
-            hi
-        </div>
+import Homepage from './Homepage'
+
+class App extends React.Component {
+
+    constructor(props){
+        super(props)
+    }
+    
+    render(){
+        return (
+            <Router>
+                <React.Fragment>
+
+                <Route exact path='/' render={(props)=>(
+                    <Homepage /> 
+                )}/>
+                    
+                </React.Fragment>
+            </Router>
     )
-}
+    }
+    
+  }
 
 export default App
